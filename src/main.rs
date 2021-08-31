6018561d500b8bc3fbdfb06702fbe90c349b6d47
Renato <renato145@hotmail.com>
2021-08-31 18:20:17 +0930

    add health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::http::Status;
+
 #[macro_use]
 extern crate rocket;
 
@@ -6,7 +8,12 @@ async fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/health_check")]
+async fn health_check() -> Status {
+    Status::Ok
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, health_check])
 }
