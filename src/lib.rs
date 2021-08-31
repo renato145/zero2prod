321bcc810efd797a4fa1baab761d0b9819851246
Renato <renato145@hotmail.com>
2021-08-31 23:45:54 +0930

    add subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use rocket::{figment::Figment, http::Status, Build, Rocket};
+use rocket::{Build, Rocket, figment::Figment, form::Form, http::Status};
 
 #[macro_use]
 extern crate rocket;
@@ -13,7 +13,18 @@ async fn health_check() -> Status {
     Status::Ok
 }
 
+#[derive(FromForm)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+#[post("/subscriptions", data = "<_form>")]
+async fn subscribe(_form: Form<FormData>) -> Status {
+    Status::Ok
+}
+
 pub fn get_rocket(config: Option<Figment>) -> Rocket<Build> {
     let figment = config.unwrap_or_else(|| Figment::from(rocket::Config::default()));
-    rocket::custom(figment).mount("/", routes![index, health_check])
+    rocket::custom(figment).mount("/", routes![index, health_check, subscribe])
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,7 @@
 use rocket::{fairing::AdHoc, figment::Figment, tokio::sync::oneshot};
 use zero2prod::get_rocket;
 
-#[rocket::async_test]
-async fn health_check_works() {
-    let port = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(format!("http://127.0.0.1:{}/health_check", port))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
-
-async fn spawn_app() -> u16 {
+async fn spawn_app() -> String {
     // Port 0 give us a random available port
     let figment = Figment::from(rocket::Config::default()).merge(("port", 0));
 
@@ -29,5 +14,65 @@ async fn spawn_app() -> u16 {
     }));
 
     rocket::tokio::spawn(server.launch());
-    rx.await.expect("Failed to get running port.")
+    let port = rx.await.expect("Failed to get running port.");
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[rocket::async_test]
+async fn health_check_works() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[rocket::async_test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let body = "name=1e%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[rocket::async_test]
+async fn subscribe_returns_client_error_when_data_is_missing() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert!(
+            response.status().is_client_error(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
