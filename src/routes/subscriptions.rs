2a777efd99b89f82a846dfae7ba5c8593a6c9b08
Renato <renato145@hotmail.com>
2021-09-01 13:26:39 +0930

    add request_id to log

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,17 @@ pub struct FormData {
 
 #[post("/subscriptions", data = "<form>")]
 pub async fn subscribe(form: Form<FormData>, db: &State<PgPool>) -> Status {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber.",
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subcriber details in the database.");
+    log::info!(
+        "request_id {} - Saving new subcriber details in the database.",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -31,11 +36,18 @@ pub async fn subscribe(form: Form<FormData>, db: &State<PgPool>) -> Status {
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved.");
+            log::info!(
+                "request_id {} - New subscriber details have been saved.",
+                request_id
+            );
             Status::Ok
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             Status::InternalServerError
         }
     }
