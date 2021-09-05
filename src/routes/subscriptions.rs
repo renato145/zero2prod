e8f9258657f5d3b11880b0a58167d15ea4eaa7f6
Renato <renato145@hotmail.com>
2021-09-05 22:20:36 +0930

    subscribe_sends_confirmation_email_with_link

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -50,12 +50,20 @@ pub async fn subscribe(
         return Status::InternalServerError;
     }
 
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
     if email_client
         .send_email(
             new_subscriber.email,
             "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
+            &format!(
+                "Welcome to our newsletter!<br />\
+                 Click <a href=\"{}\">here</a> to confirm your subscription.",
+                confirmation_link
+            ),
+            &format!(
+                "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+                confirmation_link
+            ),
         )
         .await
         .is_err()
