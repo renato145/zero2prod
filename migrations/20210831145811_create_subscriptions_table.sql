c953d33d94a668f2ee2c51e1c79773dbcabe482c
Renato <renato145@hotmail.com>
2021-09-05 21:17:02 +0930

    add subcription_tokens table

--- migrations/20210831145811_create_subscriptions_table.sql
+++ migrations/20210831145811_create_subscriptions_table.sql
@@ -1,8 +1,7 @@
--- Create Subscriptions Table
-CREATE TABLE Subscriptions (
+CREATE TABLE subscriptions (
 	id uuid NOT NULL,
 	PRIMARY KEY (id),
 	email TEXT NOT NULL UNIQUE,
 	name TEXT NOT NULL,
 	subscribed_at timestamptz NOT NULL
-)
+);
