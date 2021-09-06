3477b40b7e61caf183c3952e8ed235c4e14f3bcb
Renato <renato145@hotmail.com>
2021-09-07 01:56:19 +0930

    add ignores

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -9,7 +9,7 @@ pub struct Parameters {
 
 #[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, pool))]
 pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
-    let id = match get_subscriber_id_from_token(&**pool, &parameters.subscription_token).await {
+    let id = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
         Ok(id) => id,
         Err(_) => return HttpResponse::InternalServerError().finish(),
     };
