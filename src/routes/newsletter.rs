2e028f36fade3236b4ee38232d787a4eba6a0069
Renato <renato145@hotmail.com>
2021-09-08 01:09:18 +0930

    add password hash sha3

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -7,6 +7,7 @@ use actix_http::{
 };
 use actix_web::{web, HttpResponse, ResponseError};
 use anyhow::Context;
+use sha3::Digest;
 use sqlx::PgPool;
 
 #[derive(thiserror::Error)]
@@ -98,14 +99,16 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
 		SELECT user_id
 		FROM users
-		WHERE username = $1 AND password = $2
+		WHERE username = $1 AND password_hash = $2
 		"#,
         credentials.username,
-        credentials.password
+        password_hash
     )
     .fetch_optional(pool)
     .await
