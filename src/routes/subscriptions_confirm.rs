91afbf61e1b2a08a370b8728f7f7d83d8a5f4f36
Renato <renato145@hotmail.com>
2021-09-07 12:15:14 +0930

    add SubscribeConfirmationError

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,25 +1,48 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
+
+use crate::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     subscription_token: String,
 }
 
-#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, pool))]
-pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
-    let id = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
-        Ok(id) => id,
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    match id {
-        None => HttpResponse::Unauthorized().finish(),
-        Some(subscriber_id) => {
-            if confirm_subscriber(&pool, subscriber_id).await.is_err() {
-                return HttpResponse::InternalServerError().finish();
+#[derive(thiserror::Error)]
+pub enum SubscribeConfirmationError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for SubscribeConfirmationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for SubscribeConfirmationError {
+    fn status_code(&self) -> actix_http::StatusCode {
+        match self {
+            SubscribeConfirmationError::UnexpectedError(_) => {
+                actix_http::StatusCode::INTERNAL_SERVER_ERROR
             }
-            HttpResponse::Ok().finish()
+        }
+    }
+}
+
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, pool))]
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, SubscribeConfirmationError> {
+    let id = get_subscriber_id_from_token(&pool, &parameters.subscription_token).await?;
+    match id {
+        None => Ok(HttpResponse::Unauthorized().finish()),
+        Some(subscriber_id) => {
+            confirm_subscriber(&pool, subscriber_id).await?;
+            Ok(HttpResponse::Ok().finish())
         }
     }
 }
@@ -28,31 +51,28 @@ pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>
 pub async fn get_subscriber_id_from_token(
     pool: &PgPool,
     subscription_token: &str,
-) -> Result<Option<Uuid>, sqlx::Error> {
+) -> Result<Option<Uuid>, SubscribeConfirmationError> {
     let result = sqlx::query!(
         r#"SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1"#,
         subscription_token
     )
     .fetch_optional(pool)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .context("Failed to retrieve subscriber id from token.")?;
     Ok(result.map(|r| r.subscriber_id))
 }
 
 #[tracing::instrument(name = "Mark subscriber as confirmed", skip(subscriber_id, pool))]
-pub async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
+pub async fn confirm_subscriber(
+    pool: &PgPool,
+    subscriber_id: Uuid,
+) -> Result<(), SubscribeConfirmationError> {
     sqlx::query!(
         r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
         subscriber_id
     )
     .execute(pool)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .context("Failed to change subscrion status to 'confirmed'.")?;
     Ok(())
 }
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -19,6 +19,23 @@ async fn confirmations_without_token_are_rejected_with_404() {
 }
 
 #[actix_rt::test]
+async fn confirmations_with_invalid_token_are_rejected_with_401() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let response = reqwest::get(&format!(
+        "{}/subscriptions/confirm?subscription_token=aaaaa",
+        app.address
+    ))
+    .await
+    .unwrap();
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[actix_rt::test]
 async fn link_returned_by_subscribe_returns_200() {
     // Arrange
     let app = spawn_app().await;
@@ -73,4 +90,35 @@ async fn clicking_confirmation_link_confirms_a_subscriber() {
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
     assert_eq!(saved.status, "confirmed");
+}
+
+#[actix_rt::test]
+async fn confirmation_fails_if_there_is_a_critical_database_error() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // Sabotage database
+    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscriber_id;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // Act
+    let response = reqwest::get(confirmation_links.html)
+        .await
+        .unwrap();
+
+    // Assert
+    assert_eq!(500, response.status().as_u16());
 }
