b40b917ded687836d441b22875acafa2ad115dc0
Renato <renato145@hotmail.com>
2021-09-07 21:38:46 +0930

    add test newsletters_returns_400_for_invalid_data

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,5 +1,17 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
 
-pub async fn public_newsletter() -> HttpResponse {
+pub async fn public_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -106,3 +106,41 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
+
+#[actix_rt::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                "content": {
+                    "text": "Newsletter body as plain text",
+                    "html": "<p>Newsletter body as HTML</p>"
+                }
+            }),
+            "missing title",
+        ),
+        (
+            serde_json::json!({"title": "Newsletter title"}),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = reqwest::Client::new()
+            .post(format!("{}/newsletters", app.address))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
