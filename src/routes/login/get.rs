5e0ff7c70592e01e617d52526d3736f3632c26f9
Renato <renato145@hotmail.com>
2022-01-07 00:56:58 +1030

    login_form read cookies

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,42 +1,8 @@
-use crate::{routes::TEMPLATES, HmacSecret};
+use crate::routes::TEMPLATES;
 use actix_web::{http::header::ContentType, web, HttpResponse};
-use anyhow::Result;
-use hmac::{Hmac, Mac};
-use secrecy::ExposeSecret;
-use serde::Deserialize;
-use tracing::warn;
 
-#[derive(Deserialize)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
-
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
-        let mut mac =
-            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-        Ok(self.error)
-    }
-}
-
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error = query.map(|query| match query.0.verify(&secret) {
-        Ok(error) => error,
-        Err(e) => {
-            warn!(error.message = %e, error.caused_chain = ?e,
-                "Failed to verify query parameters using the HMAC tag"
-            );
-            "".to_string()
-        }
-    });
+pub async fn login_form(request: web::HttpRequest) -> HttpResponse {
+    let error = request.cookie("_flash").map(|c| c.value().to_string());
     let html_body = {
         let mut context = tera::Context::new();
         context.insert("error", &error);
