dd0fbe16bc4e495b770d1e7e6b66aad4e58b8681
Renato <renato145@hotmail.com>
2022-02-28 02:17:37 +1030

    add test logout_clears_session_state

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -12,3 +12,32 @@ async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
     assert_eq!(response.status().as_u16(), 303);
     assert_eq!(response.headers().get("Location").unwrap(), "/login");
 }
+
+#[tokio::test]
+async fn logout_clears_session_state() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act - Login
+    app.do_login().await;
+
+    // Act - Follow the redirect
+    let response = app.get_admin_dashboard().await;
+    let html_page = response.text().await.unwrap();
+    assert!(html_page.contains(&format!("Welcome {}", app.test_user.username)));
+
+    // Act - Logout
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), "/login");
+
+    // Act - Follow the redirect
+    let response = app.get_login().await;
+    let html_page = response.text().await.unwrap();
+    assert!(html_page.contains(r#"<p><i>You have successfully logged out.</i></p>"#));
+
+    // Act - Attempt to load admin panel
+    let response = app.get_admin_dashboard().await;
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), "/login");
+}
