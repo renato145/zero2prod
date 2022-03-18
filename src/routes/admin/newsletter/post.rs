5314be828dc73870dcbec371c3972fae4a500e22
Renato <renato145@hotmail.com>
2022-03-19 01:44:06 +1030

    add idempotency field

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -2,7 +2,8 @@ use crate::{
     authentication::UserId,
     domain::SubscriberEmail,
     email_client::EmailClient,
-    utils::{e500, see_other},
+    idempotency::IdempotencyKey,
+    utils::{e400, e500, see_other},
 };
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
@@ -16,6 +17,7 @@ pub struct FormData {
     title: String,
     text_content: String,
     html_content: String,
+    idempotency_key: String,
 }
 
 #[tracing::instrument(
@@ -29,17 +31,19 @@ pub async fn publish_newsletter(
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let FormData {
+        title,
+        text_content,
+        html_content,
+        idempotency_key,
+    } = form.0;
+    let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
         match subscriber {
             Ok(subscriber) => {
                 email_client
-                    .send_email(
-                        &subscriber.email,
-                        &form.title,
-                        &form.html_content,
-                        &form.text_content,
-                    )
+                    .send_email(&subscriber.email, &title, &html_content, &text_content)
                     .await
                     .with_context(|| {
                         format!("Failed to send newsletter issue to {}", subscriber.email)
