d49417f0bcb61eec9546d8bad3d3169ee7b51a46
Renato <renato145@hotmail.com>
2022-03-21 00:48:26 +1030

    add delay

--- examples/start_fake_email_server.rs
+++ examples/start_fake_email_server.rs
@@ -1,5 +1,4 @@
 use std::time::Duration;
-
 use wiremock::{
     matchers::{method, path},
     Mock, MockServer, ResponseTemplate,
@@ -9,6 +8,10 @@ const EMAIL_PORT: u16 = 10000;
 
 #[tokio::main]
 async fn main() {
+    let mut args = std::env::args();
+    args.next();
+    let delay = args.next().map(|s| s.parse().unwrap()).unwrap_or(2);
+    println!("Delay set to {delay} seconds.");
     println!("Starting fake email server...");
     let email_server = {
         let listener = std::net::TcpListener::bind(format!("127.0.0.1:{EMAIL_PORT}")).unwrap();
@@ -16,7 +19,7 @@ async fn main() {
     };
     let _mock_guard = Mock::given(path("/email"))
         .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
+        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(delay)))
         .mount_as_scoped(&email_server)
         .await;
     println!("Receiving connections");
